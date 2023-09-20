c62c011cda35ddbdf377b2f64998f557e177b5c8
Xinyi Sheng <111578487+xs110@users.noreply.github.com>
2023-09-20 04:05:43 +0000

    python script

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
-from script import descript_stat
-from script import save_report
-from script import readfile
+#######################################################################################
+### Python Script performing the same descriptive statistics using Polars or Panda ###
+#######################################################################################
+from lib import descript_stat
+from lib import histogram
+from lib import boxplot
+from lib import readfile
 
-
-def generate_report():
-    input_file_path = "world_population.csv"  # Update with your dataset file path
-    summary_report = "report.md"
-    histogram_image = "image/population_histogram.png"
-    boxplot_image = "image/population_boxplot.png"
-
-    WP_data = readfile(input_file_path)
-
-    # country's population in 2022, growth rate and area.
-    data = WP_data[["2022 Population", "Growth Rate", "Area (km²)"]]
-
-    descript_stat(data)
-    save_report(data, summary_report, histogram_image, boxplot_image)
 
 
 if __name__ == "__main__":
-    generate_report()
+    data = readfile('world_population.csv')
+    descript_stat(data)
+    histogram(data)
+    boxplot(data)
